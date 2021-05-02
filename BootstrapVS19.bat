326de5b4658fccb13ec762e73a1e45b8a74ec00f
Vojtěch Kužel <adalbert.kuzel@seznam.cz>
2021-05-02 20:13:55 +0200

    Added missing CALL to BootstrapVS19.bat

--- BootstrapVS19.bat
+++ BootstrapVS19.bat
@@ -17,5 +17,5 @@ IF NOT EXIST "ext\premake\bin\release\premake5.exe" (
 REM Run the premake procedure to generate a VS 2019 solution.
 REM NOTE: Uses the premake5.lua file to obtain the generation details.
 ECHO -- Configuring solution --
-ext\premake\bin\release\premake5.exe vs2019
+CALL ext\premake\bin\release\premake5.exe vs2019
 PAUSE
