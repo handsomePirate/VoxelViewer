d4aa96ec59a68c5da811fe0408af2f768bf09c62
Vojtěch Kužel <adalbert.kuzel@seznam.cz>
2021-05-08 17:14:16 +0200

    Added simple shaders

---
+++ src/Shaders/simple.comp.glsl
@@ -0,0 +1,11 @@
+#version 450
+
+layout(local_size_x = 16, local_size_y = 16) in;
+layout(binding = 0, rgba8) uniform writeonly image2D resultImage;
+
+void main()
+{
+	float green = gl_GlobalInvocationID.x / 2048.f;
+	float blue = gl_GlobalInvocationID.y / 2048.f;
+	imageStore(resultImage, ivec2(gl_GlobalInvocationID.xy), vec4(0.f, green, blue, 1.f));
+}
